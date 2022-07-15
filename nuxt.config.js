b6fd6bbce8368bfd5b2dddd9051abf64750fde72
kkodongha <87017632+kkodongha@users.noreply.github.com>
2022-07-15 10:26:28 +0900

    nuxt 기본 설정

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,25 @@
+// env별 config 파일 설정
+const defaultConfig = require('./src/config/default.json')
+let env = 'development'
+switch (process.env.NODE_ENV) {
+  case 'production':
+    env = 'production.test'
+    break
+  case 'test':
+    env = 'production.test'
+    break
+}
+
+let config = null
+try {
+  const envConfig = require(''.concat('./src/config/', env.trim(), '.json'))
+  config = Object.assign({}, defaultConfig, envConfig)
+
+  console.log(config)
+} catch (e) {
+  config = defaultConfig
+}
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -6,13 +28,25 @@ export default {
       lang: 'en',
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
+      {name: 'format-detection', content: 'telephone=no'},
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}],
   },
+
+  server: {
+    port: 3001,
+    host: '0.0.0.0', // default: localhost
+  },
+
+  env: {
+    config: config,
+    DEV_ENV: process.env.DEV_ENV,
+  },
+
+  dev: process.env.NODE_ENV !== 'production',
 
   // root src
   srcDir: 'src/',
@@ -29,21 +63,60 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    // '@nuxt/typescript-build',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
+    // https://go.nuxtjs.dev/axiosaxios
     '@nuxtjs/axios',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: `${config.appSettings.apiUrlForSSR}`,
+    browserBaseURL: `${config.appSettings.apiUrl}`,
+    init(axios) {
+      axios.defaults.withCredentials = true
+    },
+    debug: false,
+  },
+
+  router: {
+    // base: '/',
+    middleware: ['authCheck'],
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    terser: {
+      terserOptions: {
+        compress: {
+          drop_console:
+            process.env.NODE_ENV === 'development' ||
+            process.env.NODE_ENV === 'test' ||
+            process.env.DEV_ENV === 'test'
+              ? false
+              : true,
+        },
+      },
+    },
+  },
+
+  // 3rd party configuration
+  dayjs: {
+    locales: ['ko'],
+    defaultLocale: 'ko',
+    defaultTimeZone: 'Asia/Seoul',
+
+    plugins: [
+      'utc', // import 'dayjs/plugin/utc'
+      'timezone', // import 'dayjs/plugin/timezone'
+      'isBetween',
+      'isSameOrBefore',
+      'isSameOrAfter',
+      'duration',
+    ],
+  },
 }
