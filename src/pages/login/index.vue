0a8b6f1f23f8207c3e5e7f1cc663af9f34f32c90
고동하 <godongha@jinhakapply.com>
2022-10-04 09:30:47 +0900

    로그인 axios 추가

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -8,46 +8,54 @@
           <div class="card col-lg-4 mx-auto">
             <div class="card-body px-5 py-5">
               <h3 class="card-title text-left mb-3">Login</h3>
-              <form>
-                <div class="form-group">
-                  <label>Username or email *</label>
-                  <input type="text" class="form-control p_input" />
+              <div class="form-group">
+                <!-- ID -->
+                <label>Username or email *</label>
+                <input
+                  v-model="userId"
+                  type="text"
+                  class="form-control p_input"
+                />
+              </div>
+              <div class="form-group">
+                <!-- Password -->
+                <label>Password *</label>
+                <input
+                  v-model="userPassword"
+                  type="password"
+                  class="form-control p_input"
+                />
+              </div>
+              <div
+                class="form-group d-flex align-items-center justify-content-between"
+              >
+                <div class="form-check">
+                  <label class="form-check-label">
+                    <input type="checkbox" class="form-check-input" />
+                    Remember me
+                  </label>
                 </div>
-                <div class="form-group">
-                  <label>Password *</label>
-                  <input type="text" class="form-control p_input" />
-                </div>
-                <div
-                  class="form-group d-flex align-items-center justify-content-between"
+                <a href="#" class="forgot-pass">Forgot password</a>
+              </div>
+              <div class="text-center">
+                <button
+                  @click="loginSubmit"
+                  class="btn btn-primary btn-block enter-btn"
                 >
-                  <div class="form-check">
-                    <label class="form-check-label">
-                      <input type="checkbox" class="form-check-input" />
-                      Remember me
-                    </label>
-                  </div>
-                  <a href="#" class="forgot-pass">Forgot password</a>
-                </div>
-                <div class="text-center">
-                  <button
-                    type="submit"
-                    class="btn btn-primary btn-block enter-btn"
-                  >
-                    Login
-                  </button>
-                </div>
-                <div class="d-flex">
-                  <button class="btn btn-facebook mr-2 col">
-                    <i class="mdi mdi-facebook"></i> Facebook
-                  </button>
-                  <button class="btn btn-google col">
-                    <i class="mdi mdi-google-plus"></i> Google plus
-                  </button>
-                </div>
-                <p class="sign-up">
-                  Don't have an Account?<a href="#"> Sign Up</a>
-                </p>
-              </form>
+                  Login
+                </button>
+              </div>
+              <div class="d-flex">
+                <button class="btn btn-facebook mr-2 col">
+                  <i class="mdi mdi-facebook"></i> Facebook
+                </button>
+                <button class="btn btn-google col">
+                  <i class="mdi mdi-google-plus"></i> Google plus
+                </button>
+              </div>
+              <p class="sign-up">
+                Don't have an Account?<a href="#"> Sign Up</a>
+              </p>
             </div>
           </div>
         </div>
@@ -65,18 +73,30 @@ export default {
   mixins: [],
   props: {},
   data() {
-    return {}
+    return {
+      userId: null,
+      userPassword: null,
+    }
   },
-  beforeCreate() {},
+  computed: {},
+  watch: {},
   created() {},
-  beforeMount() {},
   mounted() {},
-  updated() {},
-  activated() {},
-  deactivated() {},
-  beforeDestroy() {},
   destroyed() {},
-  methods: {},
+  methods: {
+    loginSubmit() {
+      this.$axios
+        .post('/account/login', {
+          userId: this.userId,
+          userPassword: this.userPassword,
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            console.log(res)
+          }
+        })
+    },
+  },
 }
 </script>
 
